0960e7b62cc3703729d7544ff4167bffbf7ca2fe
ZOOTAA <1155049584@link.cuhk.edu.hk>
2024-09-14 21:59:32 +0800

    thresholding

--- MRIprocessing.py
+++ MRIprocessing.py
@@ -7,12 +7,14 @@ import nibabel.orientations as nio
 import matplotlib.pyplot as plt
 from skimage.restoration import denoise_nl_means,  estimate_sigma
 from skimage import filters
+import cv2
 
-def denoiseSlice(in_slice):
+def denoiseSlice(in_slice, param):
     """
     Denoise image with scikit image Non Local Means (NLM)
     Input:
         in_slice: 2d array, input image
+        param: argparse arg
     Output:
         denoise_img: 2d array
     """
@@ -30,24 +32,45 @@ def denoiseSlice(in_slice):
         )
     return denoise_img
 
-def edgefiltering(in_slice):
+def edgefiltering(in_slice, param):
     """
     Edge filtering with scikit image Sobel filter
     Input:
         in_slice: 2d array, input image
+        param: argparse arg
     Output:
         edgedetect_img: 2d array
     """
     sobel_image = filters.sobel(in_slice)
     return sobel_image
 
-def gaussianblur(in_slice, in_sigma=1):
+def gaussianblur(in_slice, param):
     """
-    
+    Gaussian Blur with scikit image Gaussian filter
+    Input:
+        in_slice: 2d array, input image
+        param: argparse arg
+    Output:
+        blurred_image: 2d array
     """
-    # Apply Gaussian Blur
-    blurred_image = filters.gaussian(in_slice, sigma=in_sigma)  # sigma defines the standard deviation
-    pass
+    blurred_image = filters.gaussian(in_slice, sigma=param.sigma)  # sigma defines the standard deviation
+    return blurred_image
+
+def thresholding(in_slice, param):
+    """
+    Thresholding of image with opencv adaptive thresholding
+    Input:
+        in_slice: 2d array, input image
+        param: argparse arg
+    Output:
+        binary_image: 2d array
+    """
+    # Normalize the image to the 0-255 range
+    normalized_image = (in_slice - param.imgmin) / (param.imgmax - param.imgmin) * 255
+    # Convert to unsigned 8-bit (uint8)
+    image_uint8 = normalized_image.astype(np.uint8)
+    binary_image = cv2.adaptiveThreshold(image_uint8, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, param.blocksize, 2)
+    return binary_image
 
 def plotSlice(in_slice, fsize=(5,5)):
     """
@@ -68,6 +91,15 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', type=str, help='file path of input data')
     parser.add_argument('-o', '--output', type=str, help='file path of output data')
     parser.add_argument('-a', '--axis', type=int, default=0, help='file path of output data')
+    parser.add_argument(
+        '-p',
+        '--process',
+        nargs='+',
+        type=str,
+        help='Image processing to do in order, support denoise, edgefilter, gaussianblur'
+    )
+    parser.add_argument('--sigma', type=float, default=1, help='sigma parameter of gaussian blur')
+    parser.add_argument('--blocksize', type=int, default=11, help='odd number block size of adaptive thresholding')
     args = parser.parse_args()
     print(args)
 
@@ -86,6 +118,18 @@ if __name__ == '__main__':
         # output file directory not exist
         print("Error: Ouput file directory not exist")
         sys.exit()
+    
+    # Processing functions
+    profunc_dict = {
+        'denoise': denoiseSlice,
+        'edgefilter': edgefiltering,
+        'gaussianblur': gaussianblur,
+        'thresholding': thresholding
+    }
+    profunc = []
+    if args.process is not None:
+        for pf in args.process:
+            profunc.append(profunc_dict[pf])
     
     # Load input data
     fext = args.input.split('.')[-2:] # get last two extension
@@ -111,11 +155,19 @@ if __name__ == '__main__':
         print("Input file type is not supported")
         sys.exit()
 
+    # max, min of image
+    image_min = np.min(img_data)
+    image_max = np.max(img_data)
+    # add to args
+    args.imgmin = image_min
+    args.imgmax = image_max
+    print('\timage minimum:', image_min, ", image maximum:", image_max)
+    
     # Plot slices
     psidx = 100
     slice0 = img_data[psidx, :, :]
     s0fig = plotSlice(slice0)
-    
+
     # Shift axis for process image along axis 0
     if args.axis > len(img_data.shape)-1:
         print('Error: Selected axis index is out of input data dimension')
@@ -127,10 +179,8 @@ if __name__ == '__main__':
     for ss in range(img_data.shape[0]):
         ppslice = img_data[ss, :, :]        
         # Image processing
-        # Denoise
-        ppslice = denoiseSlice(ppslice)
-        # Edge detection
-        ppslice = edgefiltering(ppslice)
+        for pfunc in profunc:
+            ppslice = pfunc(ppslice, args)
         # Append output
         img_data[ss, :, :] = ppslice
     print("Processing complete")
